5010fe4a30bce23a7c19ea9730b0817f11c99840
CEckelberry <cole.eckelberry@gmail.com>
2023-10-10 09:24:03 +0000

    all 3 portions into github

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ WORKDIR /app
 
 COPY ./auth /app
 COPY consumer.py /app
-RUN pip install confluent_kafka
+RUN pip install confluent_kafka click fastavro
 
 CMD ["python3", "consumer.py"]
--- consumer.py
+++ consumer.py
@@ -1,32 +1,51 @@
 from confluent_kafka import Consumer
-from os import walk
-
-filenames = next(walk(mypath), (None, None, []))[2]  # [] if no file
-
+import click
+import io
+import signal
+from fastavro import reader
 
 c = Consumer({
     'bootstrap.servers': '13.49.128.80:19093,13.49.128.80:29093,13.49.128.80:39093',
     'group.id': 'group4',
     'auto.offset.reset': 'smallest',
     'security.protocol': 'SSL',
-    'ssl.ca.location': './auth/ca.crt',
-    'ssl.keystore.location': './auth/kafka.keystore.pkcs12',
+    'ssl.ca.location': './ca.crt',
+    'ssl.keystore.location': './kafka.keystore.pkcs12',
     'ssl.keystore.password': 'cc2023',
     'enable.auto.commit': 'true',
     'ssl.endpoint.identification.algorithm': 'none',
 })
 
-c.subscribe(['group4'])
 
-while True:
-    msg = c.poll(1.0)
+def signal_handler(sig, frame):
+    print('EXITING SAFELY!')
+    exit(0)
 
-    if msg is None:
-        continue
-    if msg.error():
-        print("Consumer error: {}".format(msg.error()))
-        continue
 
-    print('Received message: {}'.format(msg.value().decode('utf-8')))
+signal.signal(signal.SIGTERM, signal_handler)
 
-c.close()
+
+@click.command()
+@click.argument('topic')
+def consume(topic: str):
+    c.subscribe(
+        [topic],
+        on_assign=lambda _, p_list: print(p_list)
+    )
+    while True:
+        msg = c.poll(1.0)
+        if msg is None:
+            continue
+        if msg.error():
+            print("Consumer error: {}".format(msg.error()))
+            continue
+        # headers = msg.headers()
+
+        message_bytes = io.BytesIO(msg.value())
+        avro_schema = reader(message_bytes)
+        print(msg.headers()[0][1].decode("utf-8"))
+        for i in avro_schema:
+            print(i)
+
+
+consume()
